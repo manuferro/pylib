2ec6346339cd256c2ce47ac8decdfbc836997f91
manuferro <ferro1@libero.it>
2019-01-22 15:54:16 +0100

    just a test of commit

--- class_use_test.py
+++ class_use_test.py
@@ -3,7 +3,7 @@
 import class_test
 
 if __name__ == '__main__':
-    print "class use test"
+    print "class use test (modifica da VisualStudio)"
 
     obj = class_test.TestClass()
     print obj.v
